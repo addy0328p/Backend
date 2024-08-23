3bb3bf5861625d29ffc27fe4c206f782473162a4
Aditya Pandey <addy@example.com>
2024-08-24 02:55:25 +0530

    revising backend

---
+++ hello.js
@@ -0,0 +1,11 @@
+console.log('Hello world');
+// All the DOM manipulation is removed like window objects in node.Only core functiaonlitie are added to perfom core.
+
+//npm init create file to run the project.
+
+// npm init creates package.json files in the   project directory.
+// This is the package.json file for a Node.js project named "backend".
+// It contains metadata about the project, including its name, version, scripts, author, license, and description.
+// The "scripts" section defines two scripts: "start" and "test".
+// The "start" script runs "node hello.js", which means it will start the application defined in the "hello.js" file.
+// The "test" script currently just prints an error message and exits with a status code of 1, indicating that no tests are specified.
